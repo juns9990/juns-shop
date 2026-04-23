805e9213070650265398a02ca9d3593c20d7a5e7
juns9990 <juns@kakao.com>
2026-04-23 21:47:19 +0900

    Add files via upload

---
+++ juns-sw.js
@@ -0,0 +1,43 @@
+var CACHE = 'juns-v1.9.3';
+var ASSETS = [
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './juns-manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(ASSETS);
+    }).catch(function() {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      if (response && response.status === 200) {
+        var clone = response.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
